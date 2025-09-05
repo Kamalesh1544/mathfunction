7544174d2caa9f328a5e8e149f386bc712547cb2
Kamalesh1544 <kamaleshjohnson7@gmail.com>
2025-09-05 11:36:13 -0700

    c

--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,10 @@
 import pytest 
 from main import add
 
-@pytest.mark.parametrize("a,b,expected",[
-    (2,3,5),
-    (1,1,2),
-    (1,2,4)
+@pytest.mark.parametrize("a,b,expected", [
+    (2, 3, 5),
+    (1, 1, 2),
+    (1, 2, 3)  # corrected
 ])
-def test_add(a,b,expected):
-    assert add(a,b)==expected
+def test_add(a, b, expected):
+    assert add(a, b) == expected
