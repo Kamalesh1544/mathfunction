db9f1a0952d78b64ca86cc69217d30f952ecbd78
Kamalesh1544 <kamaleshjohnson7@gmail.com>
2025-09-05 23:39:32 -0700

    n

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import add
 @pytest.mark.parametrize("a,b,expected", [
     (2, 3, 5),
     (1, 1, 2),
-    (1, 2, 3)  # corrected
+    (1, 2, 3)  
 ])
 def test_add(a, b, expected):
     assert add(a, b) == expected
